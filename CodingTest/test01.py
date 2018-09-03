5e4938c08d40191ec23c545983a4a73238cb3237
김태훈 <niceman555@gamil.com>
2018-09-04 00:51:30 +0900

    코딩테스트01

---
+++ CodingTest/test01.py
@@ -0,0 +1,14 @@
+# s = "133303"
+# answer = []
+# for i in s:
+#     print(answer[-1:])
+#     if answer[-1:] == [i]:
+#         continue
+#     answer.append(i)
+#
+# print(answer)
+#
+# lst=[1,3,4]
+# print(lst[-1:])
+s = "abcde"
+print(s[2:3])
