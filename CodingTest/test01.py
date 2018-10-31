07475edb34ebf189948bd2f88ce99c12fd561267
김태훈 <niceman555@gamil.com>
2018-10-31 23:46:00 +0900

    Revert "집합관련 테스트"
    This reverts commit 312c3ab93305bd2f6a526564a7f67986fd1261e7.

--- CodingTest/test01.py
+++ CodingTest/test01.py
@@ -10,31 +10,5 @@
 #
 # lst=[1,3,4]
 # print(lst[-1:])
-
-# s = "abcde"
-# print(s[2:3])
-
-
-import collections
-
-lst1 = ['a','b','c','a','f','g']
-tup1 = ['b','d','e','e','f','f','g','h']
-
-c1 = collections.Counter(lst1)
-c2 = collections.Counter(tup1)
-
-# 합집합
-print(c1+c2)                    
-print(list((c1+c2).elements()))
-
-# 교집합
-print(c1&c2)
-print(list((c1&c2).elements()))
-
-# 차집합
-print(c1-c2)
-print(list((c1-c2).elements()))
-
-# 최대값
-print(c1|c2)
-print(list((c1|c2).elements()))
+s = "abcde"
+print(s[2:3])
