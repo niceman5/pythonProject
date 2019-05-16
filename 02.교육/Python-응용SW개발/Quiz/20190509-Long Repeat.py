2a3291e86a094128052e79f9a82b2ab5df26c527
niceman555 <niceman555@gmail.com>
2019-05-17 00:09:29 +0900

    no message

--- 02.교육/Python-응용SW개발/Quiz/20190509-Long Repeat.py
+++ 02.교육/Python-응용SW개발/Quiz/20190509-Long Repeat.py
@@ -4,25 +4,52 @@
 
 
 def long_repeat(line):
-
-    max_len = 0
-    tmp_len = 0
+    max_char = ''
     tmp_char = ''
+
+
     for v in line:
-        if tmp_char != v:
-            if tmp_len > max_len:
-                max_len = tmp_len
-            tmp_len, tmp_char = 0, v
-        else:
-            tmp_len += 1
-    print(max_len)
-    return max_len
+        if len(tmp_char) > 0 and tmp_char[-1] != v:
+            if len(tmp_char) > len(max_char):
+                max_char = tmp_char
+            tmp_char = ''
+        tmp_char = tmp_char + v
+    # print(tmp_char,max_char)
+    if len(max_char) == 0:
+        max_char = tmp_char
+
+    return len(max_char)
+
+
+
+from itertools import groupby
+
+# def long_repeat(line):
+#     return max((sum(1 for _ in g) for k, g in groupby(line)), default=0)
+#
+
+
+# def long_repeat(line):
+#     m = 0
+#     p = 0
+#     for x in range(len(line)):
+#         if x < len(line) - 1:
+#             if line[x] == line[x + 1]:
+#                 p += 1
+#             else:
+#                 p = 0
+#         if p + 1 > m:
+#             m = p + 1
+#
+#     # your code here
+#     return m
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert long_repeat('sdsffffse') == 4, "First"
-    assert long_repeat('ddvvrwwwrggg') == 3, "Second"
-    assert long_repeat('abababaab') == 2, "Third"
-    assert long_repeat('') == 0, "Empty"
+    # assert long_repeat('sdsffffse') == 4, "First"
+    # assert long_repeat('ddvvrwwwrggg') == 3, "Second"
+    # assert long_repeat('abababaab') == 2, "Third"
+    # assert long_repeat('') == 0, "Empty"
+    assert long_repeat('aa') == 2, "2"
     print('"Run" is good. How is "Check"?')
 
