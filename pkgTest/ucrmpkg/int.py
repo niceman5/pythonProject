bfbb75a5f8d85d64ac5fe65285fed672f1f401c5
niceman555 <niceman555@gmail.com>
2023-05-21 13:46:41 +0900

    package 0.1

---
+++ pkgTest/ucrmpkg/int.py
@@ -0,0 +1,20 @@
+""" 숫자형 관련 확장함수
+"""
+def get_first_int(values, key, default=0):
+    """dict에 있는 key값을 검색해서 있으면 해당 값을 리턴하고 없으면 default값을 return
+    
+    Args :
+        values  : 대상 dict
+        key : 찾으려는 key
+        default : 기본 리턴값
+        
+    Returs : 
+        dict에 있는 기본 key에 value값을 리턴. 없으면 default값을 리턴
+    
+    """
+    found = values.get(key, [''])
+    if found[0] :
+        found = int(found[0])
+    else:
+        found = default
+    return found
