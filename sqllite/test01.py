d4e9217a30e720e0d533979b9fa1df1f484bbb43
niceman555 <niceman555@gmail.com>
2023-06-07 11:33:26 +0900

    sqllite2

--- sqllite/test01.py
+++ sqllite/test01.py
@@ -1,5 +1,31 @@
 import sqlite3
+from enum import Enum
 
+class Dessert(Enum):
+    ID = 0
+    name = 1
+    kal = 2
+    
+    
+# Decorator
+def constant(func):
+    def func_set(self, value):
+        raise TypeError
+
+    def func_get(self):
+        return func()
+    return property(func_get, func_set)
+
+class _Dessert():
+    @constant
+    def ID():
+        return 0
+    @constant
+    def NAME():
+        return 1
+    @constant
+    def KAL():
+        return 2
 # 메모리에다가 DB서버를 구성(연결)하기
 con = sqlite3.connect(':memory:')
 
@@ -14,8 +40,10 @@ cur.execute("insert into dessert(id,name,kal) values(3,'케이크3',33)")
 
 cur.execute("select * from dessert")
 
-row = cur.fetchall()
-print(row, type(row))
+rows = cur.fetchall()
+print(rows, type(rows))
+for row in rows:
+    print(row, type(row))
 
 # drink = DataFrame({'id':[1,2,3],'name' : ['콜라','사이다','커피'],'kcal': [37,44,1]})
 # drink.to_sql('drink',con,index=False)
@@ -30,6 +58,18 @@ print(row, type(row))
 # cur.fetchall()
 # rows = cur.fetchall()
 # print(rows, type(rows))
+
+
+dessert = _Dessert()
+print(dessert.ID , type(dessert.ID))
+for row in cur.execute("select * from dessert"):
+    print(row, type(row))   # 튜플로 
+    # print(Dessert.ID, type(Dessert.ID))
+    print(row[dessert.ID], type(dessert.ID))
+    print(row[dessert.NAME], type(dessert.NAME))
+    print(row[dessert.KAL], type(dessert.KAL))
+    
+    
 
 
 cur.close()
@@ -60,5 +100,16 @@ cur.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
 # 직접쿼리를 사용할수 있음
 for row in cur.execute('SELECT * FROM stocks ORDER BY price'):
     print(row)
+
+
+# update 
+# 방법 1
+c.execute("UPDATE table1 SET name=? WHERE id=?", ('NEW1', 1))
+
+# 방법 2
+c.execute("UPDATE table1 SET name=:name WHERE id=:id", {"name": 'NEW2', 'id': 3})
+
+# 방법 3
+c.execute("UPDATE table1 SET name='%s' WHERE id='%s'" % ('NEW3', 5))
 
 '''
