f2a2e6bbf33b2222f94f23888716208dd610c43e
DESKTOP-N3RMADT\김태훈 <niceman555@gmail.com>
2024-12-02 17:17:13 +0900

    개발테스트분임

---
+++ Test/testcode.py
@@ -0,0 +1,15 @@
+print('{:02d}'.format(1))
+
+
+
+# for i in range(2024, 2025):
+#     print(i)
+#     for j in range(1, 13):
+#         print(j)
+
+
+d = {}  # dict선언
+d = {'alice': [1, 2, 3], 'bob': 20, 'tony': 15, 'suzy': 30}
+print(d['alice'])
+print(d['bob'])
+
